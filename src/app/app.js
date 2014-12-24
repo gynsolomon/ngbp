0161a336682362156771cd6fd0b49881349153a9
Yanan Guo <gynsolomon@gmail.com>
2014-12-24 11:39:02 +0800

    add course builder hello world page

--- src/app/app.js
+++ src/app/app.js
@@ -1,25 +1,23 @@
-angular.module( 'ngBoilerplate', [
-  'templates-app',
-  'templates-common',
-  'ngBoilerplate.home',
-  'ngBoilerplate.about',
-  'ui.router'
+/**
+ * Created by solomon on 14/12/24.
+ */
+angular.module('ycmath', [
+    'templates-app',
+    'templates-common',
+    'ycmath.editor',
+    'ui.router'
 ])
-
-.config( function myAppConfig ( $stateProvider, $urlRouterProvider ) {
-  $urlRouterProvider.otherwise( '/home' );
+.config(function myAppConfig($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/editor');
 })
 
-.run( function run () {
+.run(function run() {
 })
 
-.controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
-  $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
-    if ( angular.isDefined( toState.data.pageTitle ) ) {
-      $scope.pageTitle = toState.data.pageTitle + ' | ngBoilerplate' ;
-    }
-  });
-})
-
-;
-
+.controller('AppCtrl', function AppCtrl($scope, $location) {
+    $scope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
+        if (angular.isDefined(toState.data.pageTitle)) {
+            $scope.pageTitle = toState.data.pageTitle + ' | 洋葱数学';
+        }
+    });
+});
