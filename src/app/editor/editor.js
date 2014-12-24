71035085a4ba0b94bf38d143fc69745b870b3e93
Yanan Guo <gynsolomon@gmail.com>
2014-12-24 11:55:52 +0800

    correct config for editor's hw page

---
+++ src/app/editor/editor.js
@@ -0,0 +1,23 @@
+/**
+ * Created by solomon on 14/12/24.
+ */
+angular.module( 'ycmath.editor', [
+    'ui.router'
+])
+
+.config(function config( $stateProvider ) {
+    $stateProvider.state( 'editor', {
+        url: '/editor',
+        views: {
+            "main": {
+                controller: 'EditorCtrl',
+                templateUrl: 'editor/editor.tpl.html'
+            }
+        },
+        data:{ pageTitle: '课程编辑后台' }
+    });
+})
+
+.controller( 'EditorCtrl', function EditorCtrl( $scope ) {
+
+});
